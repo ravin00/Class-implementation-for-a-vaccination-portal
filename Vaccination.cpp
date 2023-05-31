4a697bde4a99f0bcca7c74d3caf34114625f130c
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2023-05-31 17:53:14 +0530

    Create Vaccination.cpp

---
+++ Vaccination.cpp
@@ -0,0 +1,14 @@
+void VaccinationCenter::setVaccinationCenterDetails(int cID, string cName, string cAddress, string cPhone, string cEmail, string cPersonName,
+string cPersonPhone, float cLatitude, float cLongitude) {
+          center_id = cID;
+          center_name = cName;
+          address = cAddress;
+          phone = cPhone;
+          email = cEmail;
+          contact_person_name = cPersonName;
+          contact_person_phone = cPersonPhone;
+          latitude = cLatitude;
+      longitude = cLatitude;
+void VaccinationCenter::displayVaccinationCenterDetails() {
+}
+}
