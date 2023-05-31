0bb0e82f6a1a4301993c92d5dc2c0148f02252ef
Uthsara K A R <134998461+ravin00@users.noreply.github.com>
2023-05-31 17:54:01 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+int main() {
+VaccinationCenter c1, c2, c3, c4, c5; pg. 14
+c1.setVaccinationCenterDetails(1, "Thevin", "243/B Makola", "0777448428", "thev.sami@gmail.com", "John", "0776085874", 40.7128, -97.0060);
+c2.setVaccinationCenterDetails(2, "Ravin", "243/B Piliyandala", "0777448728", "ravin@gmail.com", "David", "0779875874", 78.5628, -74.0060);
+c3.setVaccinationCenterDetails(3, "Ajey", "243/B kandy", "0777445628", "ajey@gmail.com", "Andrew", "0776098674", 25.7789, -56.0060);
+c4.setVaccinationCenterDetails(4, "Thiwanka", "243/B Dambulla", "0778448428", "thiwanka@gmail.com", "Liam", "0776954874", 89.7642, -65.0060);
+c5.setVaccinationCenterDetails(5, "Sithmi", "243/B Malabe", "0777898428", "sithmi@gmail.com", "rozaan", "0776943874", 36.8965, -75.0060);
+          c1.displayVaccinationCenterDetails();
+          c2.displayVaccinationCenterDetails();
+          c3.displayVaccinationCenterDetails();
+          c4.displayVaccinationCenterDetails();
+          c5.displayVaccinationCenterDetails();
+return 0; }
